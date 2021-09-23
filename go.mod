623fe921c782fc3d4c8c91c84c6d34f13289450d
Виталий <vitalyerhsov2001@gmail.com>
2021-09-23 22:17:23 +0300

    LOS-18: Попробовал сделать роут signin + подключил БД

--- go.mod
+++ go.mod
@@ -2,14 +2,13 @@ module 2021_2_LostPointer
 
 go 1.17
 
-require (
-	github.com/labstack/echo/v4 v4.6.0
-)
+require github.com/labstack/echo/v4 v4.6.0
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.10.3 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"2021_2_LostPointer/handlers"
+	"database/sql"
+	"fmt"
 	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 func main() {
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DBUSER"), os.Getenv("DBPASS"), os.Getenv("DBHOST"), os.Getenv("DBPORT"),
+		os.Getenv("DBNAME"))
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
-
 	e.GET("/", handlers.GetHomePageHandler())
-	e.POST("/signup", handlers.CreateUserHandler())
-	e.POST("/signin", handlers.LoginUserHandler())
-
+	e.POST("/signup", handlers.CreateUserHandler(db))
+	e.POST("/signin", handlers.LoginUserHandler(db)) // This one works
 	e.Logger.Fatal(e.Start(":3030"))
 }

279acac280b2cf55b317360e83b862b3b802962a
Виталий <vitalyerhsov2001@gmail.com>
2021-09-23 20:20:12 +0300

    LOS-18: Сделал сервер с заглушками

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module 2021_2_LostPointer
 
 go 1.17
 
-require github.com/labstack/echo/v4 v4.6.0
+require (
+	github.com/labstack/echo/v4 v4.6.0
+)
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+	"2021_2_LostPointer/handlers"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	e.GET("/", handlers.GetHomePageHandler())
+	e.POST("/signup", handlers.CreateUserHandler())
+	e.POST("/signin", handlers.LoginUserHandler())
 
-	// Routes
-	e.GET("/", hello)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	e.Logger.Fatal(e.Start(":3030"))
 }

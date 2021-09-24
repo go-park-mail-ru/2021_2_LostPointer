b882a597e7cf2cea1dee53597deedb834d0e72ff
Виталий <vitalyerhsov2001@gmail.com>
2021-09-24 23:22:16 +0300

    LOS-18: Переписал логику роутов /signin и /signup

--- models/user.go
+++ models/user.go
@@ -1,42 +1,62 @@
 package models
 
 import (
-	"crypto/sha1"
+	"2021_2_LostPointer/utils"
 	"database/sql"
-	"encoding/hex"
 )
 
+const Salt = "abEdfg" // Соль, которая добавляется к паролю
+
 type User struct {
-	Login string `json:"id"`
-	Password string `json:"name"`
+	ID int
+	Username string
+	Password string
 }
 
-// Password hashing
-func getHash(str string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(str))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-// IsUserExists - check if user exists in db
-func IsUserExists(db *sql.DB, user *User) (bool, error) {
-	hashedPassword := getHash(user.Password)
-
-	rows, err := db.Query(
-		`SELECT EXISTS(SELECT id FROM users WHERE
-			username = $1 AND password = $2)`,
-		user.Login, hashedPassword)
+// UserExistsLogin - используется обработчиком LoginUserHandler. Проверяет, что пользователь,
+// который пытается авторизоваться есть в базе данных.
+func UserExistsLogin(db *sql.DB, user User) (bool, error) {
+	rows, err := db.Query(`SELECT id, username, password FROM users
+			WHERE username=$1`, user.Username)
 	if err != nil {
 		return false, err
 	}
 	defer rows.Close()
 
-	var flag bool
-	for rows.Next() {
-		if err := rows.Scan(&flag); err != nil {
-			return false, err
-		}
+	if !rows.Next() { // Из базы пришел пустой запрос => пользователя в базе данных нет
+		return false, nil
+	}
+	var u User
+	if err := rows.Scan(&u.ID, &u.Username, &u.Password); err != nil {
+		return false, err
 	}
 
-	return flag, nil
+	if utils.GetHash(user.Password + Salt) != u.Password { // Пароли не совпадают
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// IsUserUnique - используется обработчиком SignUpHandler. Проверяет что пользователь с указанным
+// при регистрации username уникален.
+func IsUserUnique(db *sql.DB, user User) (bool, error) {
+	rows, err := db.Query(`SELECT * FROM users WHERE username=$1`, user.Username)
+	if err != nil {
+		return false, err
+	}
+	if rows.Next() { // Пользователь с таким username зарегистрирован
+		return false, nil
+	}
+	return true, nil
+}
+
+// CreateUser - создаем пользователя в базе
+func CreateUser(db *sql.DB, user User) error {
+	_, err := db.Exec(`INSERT INTO users(username, password)
+			VALUES($1, $2)`, user.Username, utils.GetHash(user.Password + Salt))
+	if err != nil {
+		return err
+	}
+	return nil
 }

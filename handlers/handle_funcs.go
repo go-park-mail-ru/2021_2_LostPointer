2874ec71e6fcd2b8d59a39ae98d2ddf0f3fb13d6
Виталий <vitalyerhsov2001@gmail.com>
2021-09-25 16:59:47 +0300

    LOS-18: Добавил хранение сессий

--- handlers/handle_funcs.go
+++ handlers/handle_funcs.go
@@ -2,27 +2,30 @@ package handlers
 
 import (
 	"2021_2_LostPointer/models"
+	"2021_2_LostPointer/utils"
 	"database/sql"
+	"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 	uuid "github.com/satori/go.uuid"
+	"log"
 	"net/http"
 	"time"
 )
 
-func LoginUserHandler(db *sql.DB) echo.HandlerFunc{
+func LoginUserHandler(db *sql.DB, redisConnection *redis.Client) echo.HandlerFunc{
 	return func(c echo.Context) error {
 		var user models.User
 		if err := c.Bind(&user); err != nil {
 			return err
 		}
-		userExists, err := models.UserExistsLogin(db, user)
+		userID, err := utils.UserExistsLogin(db, user)
 		if err != nil {
 			return err
 		}
-
-		if !userExists {
+		if userID == 0 {
 			return c.JSON(http.StatusNotFound, "ERROR: User not found")
 		}
+
 		sessionToken, err := uuid.NewV4()
 		if err != nil {
 			return err
@@ -31,19 +34,26 @@ func LoginUserHandler(db *sql.DB) echo.HandlerFunc{
 		cookie.Name = "Session_cookie"
 		cookie.Value = sessionToken.String()
 		cookie.HttpOnly = true
-		cookie.Expires = time.Now().Add(365 * 24 * time.Hour)
+		cookie.Expires = time.Now().Add(time.Hour)
 		c.SetCookie(cookie)
+
+		err = utils.StoreSession(redisConnection,
+			&models.Session{UserID: userID, Session: cookie.Value})
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		return c.JSON(http.StatusOK, "OK: We can authorize user")
 	}
 }
 
-func SignUpHandler(db *sql.DB) echo.HandlerFunc {
+func SignUpHandler(db *sql.DB, redisConnection *redis.Client) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var user models.User
 		if err := c.Bind(&user); err != nil {
 			return err
 		}
-		isUnique, err := models.IsUserUnique(db, user)
+		isUnique, err := utils.IsUserUnique(db, user)
 		if err != nil {
 			return err
 		}
@@ -51,10 +61,11 @@ func SignUpHandler(db *sql.DB) echo.HandlerFunc {
 		if !isUnique {
 			return c.JSON(http.StatusBadRequest, "ERROR: User is not unique")
 		}
-		err = models.CreateUser(db, user)
+		userID, err := utils.CreateUser(db, user)
 		if err != nil {
 			return err
 		}
+
 		sessionToken, err := uuid.NewV4()
 		if err != nil {
 			return err
@@ -63,8 +74,15 @@ func SignUpHandler(db *sql.DB) echo.HandlerFunc {
 		cookie.Name = "Session_cookie"
 		cookie.Value = sessionToken.String()
 		cookie.HttpOnly = true
-		cookie.Expires = time.Now().Add(365 * 24 * time.Hour)
+		cookie.Expires = time.Now().Add(time.Hour)
 		c.SetCookie(cookie)
+
+		err = utils.StoreSession(redisConnection,
+			&models.Session{UserID: userID, Session: cookie.Value})
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		return c.JSON(http.StatusCreated, "OK: User created")
 	}
 }

a79016a1f96911eff03dec15fe602264dc202156
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-09-28 12:57:44 +0300

    Add music to selection for home page

--- models/music.go
+++ models/music.go
@@ -5,61 +5,103 @@ import (
 	"fmt"
 )
 
-const tracksSelectionLimit = 50
+const tracksSelectionLimit = 10
+const albumsSelectionLimit = 4
 const playlistsSelectionLimit = 4
 const artistsSelectionLimit = 4
 
 type Track struct {
-	Id          int64
-	Title       string
-	Artist      string
-	Album       string
-	Explicit    bool
-	Genre       string
-	Number      int32
-	File        string
-	ListenCount uint64
+	Id          int64  `json:"id"`
+	Title       string `json:"title"`
+	Artist      string `json:"artist"`
+	Album       string `json:"album"`
+	Explicit    bool   `json:"explicit"`
+	Genre       string `json:"genre"`
+	Number      int64  `json:"number"`
+	File        string `json:"file"`
+	ListenCount int64  `json:"listenCount"`
+	Duration    int64  `json:"duration"`
+	Cover       string `json:"cover"`
+}
+
+type Album struct {
+	Id             int64  `json:"id"`
+	Title          string `json:"title"`
+	Year           int64  `json:"year"`
+	Artist         string `json:"artist"`
+	ArtWork        string `json:"artWork"`
+	TrackCount     int64  `json:"trackCount"`
+	TracksDuration int64  `json:"tracks_duration"`
 }
 
 type Artist struct {
-	Id   int64
-	Name string
-	Bio  string
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	Bio  string `json:"bio"`
 }
 
 type Playlist struct {
-	Id   int64
-	Name string
-	User int64
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	User int64  `json:"user"`
 }
 
 type SelectionFroHomePage struct {
-	Tracks    []Track
-	Playlists []Playlist
-	Artists   []Artist
+	Tracks    []Track    `json:"tracks"`
+	Albums    []Album    `json:"albums"`
+	Playlists []Playlist `json:"playlists"`
+	Artists   []Artist   `json:"artists"`
 }
 
 func GetTracks(amount int, db *sql.DB) ([]Track, error) {
 	tracks := make([]Track, 0)
-	rows, err := db.Query(fmt.Sprintf(`SELECT DISTINCT ON(alb.title, g.name) t.id, t.title, art.name, alb.title,  t.explicit, g.name, t.number, t.file, t.listen_count FROM tracks t
+	rows, err := db.Query(fmt.Sprintf(`SELECT DISTINCT ON(alb.title, g.name) t.id, t.title, art.name, alb.title,
+												t.explicit, g.name, t.number, t.file, t.listen_count, t.duration, alb.artwork FROM tracks t
     											LEFT JOIN albums alb ON alb.id = t.album
     											LEFT JOIN artists art ON art.id = t.artist
     											LEFT JOIN genres g ON g.id = t.genre
     											LIMIT %d`, amount))
 	if err != nil {
+		fmt.Println(tracks)
 		return nil, err
 	}
 
 	var track Track
 	for rows.Next() {
 		if err := rows.Scan(&track.Id, &track.Title, &track.Artist, &track.Album, &track.Explicit, &track.Genre,
-			&track.Number, &track.File, &track.ListenCount); err != nil {
+			&track.Number, &track.File, &track.ListenCount, &track.Duration, &track.Cover); err != nil {
+			fmt.Println(err)
+
 			return nil, err
 		}
 		tracks = append(tracks, track)
 	}
 
 	return tracks, nil
+}
+
+func GetAlbums(amount int, db *sql.DB) ([]Album, error) {
+	albums := make([]Album, 0)
+	rows, err := db.Query(fmt.Sprintf(`SELECT a.id, a.title, a.year, art.name, a.artwork, a.track_count, SUM(t.duration) as tracksDuration FROM albums a
+    											LEFT JOIN artists art ON art.id = a.artist
+    											JOIN tracks t on t.album = a.id
+    											GROUP BY a.id, a.title, a.year, art.name, a.artwork, a.track_count LIMIT %d`, amount))
+	if err != nil {
+		fmt.Println(err)
+
+		return nil, err
+	}
+
+	var album Album
+	for rows.Next() {
+		if err := rows.Scan(&album.Id, &album.Title, &album.Year, &album.Artist, &album.ArtWork, &album.TrackCount, &album.TracksDuration); err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		albums = append(albums, album)
+	}
+
+	return albums, nil
 }
 
 func GetPlaylists(amount int, db *sql.DB) ([]Playlist, error) {
@@ -106,6 +148,9 @@ func GetSelectionForHomePage(db *sql.DB) (*SelectionFroHomePage, error) {
 	var err error
 
 	if selectionForHomePage.Tracks, err = GetTracks(tracksSelectionLimit, db); err != nil {
+		return nil, err
+	}
+	if selectionForHomePage.Albums, err = GetAlbums(albumsSelectionLimit, db); err != nil {
 		return nil, err
 	}
 	if selectionForHomePage.Playlists, err = GetPlaylists(playlistsSelectionLimit, db); err != nil {

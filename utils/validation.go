6697fcc4e35bfeb9c1c6bbc466443974d876da90
Виталий <vitalyerhsov2001@gmail.com>
2021-10-01 17:53:45 +0300

    Сделал валидацию при регистрации

--- utils/validation.go
+++ utils/validation.go
@@ -5,14 +5,40 @@ import (
 	"regexp"
 )
 
+func passwordValidation(password string) (bool, error) {
+	isLong := len(password) > 8
+	containDigit, err := regexp.MatchString(`[0-9]`, password)
+	if err != nil {
+		return false, err
+	}
+	containLower, err := regexp.MatchString(`[a-z]`, password)
+	if err != nil {
+		return false, err
+	}
+	containUpper, err := regexp.MatchString(`[A-Z]`, password)
+	if err != nil {
+		return false, err
+	}
+	containSpecial, err := regexp.MatchString(`[\!\@\#\$\%\^\&\*]`, password)
+	if err != nil {
+		return false, err
+	}
+	return isLong && containDigit && containLower && containUpper && containSpecial, nil
+}
+
 func ValidateSignUp(user *models.User) (bool, error) {
 	nameValid, err := regexp.MatchString(`^([a-zA-Z]{2,15})$`, user.Name)
-	/*log.Println("Name:", nameValid)*/
 	if err != nil {
 		return false, err
 	}
 	usernameValid, err := regexp.MatchString(`[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+`, user.Username)
-	/*log.Println("Username", usernameValid)*/
+	if err != nil {
+		return false, err
+	}
+	passwordValid, err := passwordValidation(user.Password)
+	if err != nil {
+		return false, err
+	}
 
-	return nameValid && usernameValid, nil
+	return nameValid && usernameValid && passwordValid, nil
 }

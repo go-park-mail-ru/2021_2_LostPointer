b6ae210dc66914622297b29c5648888d35d7fcce
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-10-08 00:59:03 +0300

    Selections with fixes

--- utils/musicSelection_test.go
+++ utils/musicSelection_test.go
@@ -9,6 +9,93 @@ import (
 	"testing"
 )
 
+func TestCreateTracksRequestWithGenres(t *testing.T) {
+	result := createTracksRequestWithGenres([]string{"Pop", "Jazz"})
+	expected := `SELECT DISTINCT ON(tracks.album) tracks.id FROM tracks
+LEFT JOIN genres g on tracks.genre = g.id
+WHERE g.name='Pop' OR g.name='Jazz'`
+
+	assert.Equal(t, expected, result)
+}
+
+func TestCreateTracksRequestWithIds(t *testing.T) {
+	result := createTracksRequestWithIds([]int{1799, 1800})
+	expected := `SELECT t.id,
+							t.title,
+							art.name,
+							alb.title,
+							t.explicit,
+							g.name,
+							t.number,
+							t.file,
+							t.listen_count,
+							t.duration,
+							alb.artwork
+							FROM tracks t
+							LEFT JOIN albums alb ON alb.id = t.album
+							LEFT JOIN artists art ON art.id = t.artist
+							LEFT JOIN genres g ON g.id = t.genre
+							WHERE t.id IN (1799, 1800)`
+
+	assert.Equal(t, expected, result)
+}
+
+func TestGetTracksIdGenre(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
+		SELECT name FROM genres
+	`))).WillReturnRows(func() *sqlmock.Rows {
+		row := sqlmock.NewRows([]string{"name"})
+		row.AddRow("Pop")
+		return row
+	}())
+
+	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
+		SELECT DISTINCT ON(tracks.album) tracks.id FROM tracks
+LEFT JOIN genres g on tracks.genre = g.id
+WHERE g.name='Pop'
+	`))).WillReturnRows(func() *sqlmock.Rows {
+		row := sqlmock.NewRows([]string{"id"})
+		row.AddRow(1799)
+		return row
+	}())
+
+	result, err := getTracksIdByGenre(db, []string{"Pop"})
+	if err != nil {
+		t.Errorf("Error %s\n occurred during test case with %d\n", err, 1799)
+	}
+
+	assert.Equal(t, []int{1799}, result)
+}
+
+func TestIsGenreExist(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
+		SELECT name FROM genres
+	`))).WillReturnRows(func() *sqlmock.Rows {
+		row := sqlmock.NewRows([]string{"name"})
+		row.AddRow("Pop")
+		return row
+	}())
+
+	result, err := isGenresExist([]string{"Pop"}, db)
+	if err != nil {
+		t.Errorf("Error %s\n occurred during test case with %d\n", err, 1799)
+	}
+
+	assert.Equal(t, true, result)
+}
+
 func TestGetTracks(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -33,12 +120,41 @@ func TestGetTracks(t *testing.T) {
 	tracks = append(tracks, track)
 
 	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
-		SELECT DISTINCT ON(alb.title, g.name) t.id, t.title, art.name, alb.title,
-		t.explicit, g.name, t.number, t.file, t.listen_count, t.duration, alb.artwork FROM tracks t
-		LEFT JOIN albums alb ON alb.id = t.album
-		LEFT JOIN artists art ON art.id = t.artist
-		LEFT JOIN genres g ON g.id = t.genre
-		LIMIT 1
+		SELECT name FROM genres
+	`))).WillReturnRows(func() *sqlmock.Rows {
+		row := sqlmock.NewRows([]string{"name"})
+		row.AddRow("Pop")
+		return row
+	}())
+
+	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
+		SELECT DISTINCT ON(tracks.album) tracks.id FROM tracks
+LEFT JOIN genres g on tracks.genre = g.id
+WHERE g.name='Pop'
+	`))).WillReturnRows(func() *sqlmock.Rows {
+		row := sqlmock.NewRows([]string{"id"})
+		row.AddRow(1799)
+		row.AddRow(1800)
+		return row
+	}())
+
+	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
+		SELECT t.id,
+							t.title,
+							art.name,
+							alb.title,
+							t.explicit,
+							g.name,
+							t.number,
+							t.file,
+							t.listen_count,
+							t.duration,
+							alb.artwork
+							FROM tracks t
+							LEFT JOIN albums alb ON alb.id = t.album
+							LEFT JOIN artists art ON art.id = t.artist
+							LEFT JOIN genres g ON g.id = t.genre
+							WHERE t.id IN (1799)
 	`))).WillReturnRows(func() *sqlmock.Rows {
 		row := sqlmock.NewRows([]string{"id", "title", "artist", "album", "explicit", "genre", "number",
 			"file", "listen_count", "duration", "artwork"})
@@ -47,7 +163,7 @@ func TestGetTracks(t *testing.T) {
 		return row
 	}())
 
-	result, err := GetTracks(1, db)
+	result, err := GetTracks(1, db, "Pop")
 	if err != nil {
 		t.Errorf("Error %s\n occurred during test case with %+v\n", err, track)
 	}
@@ -66,7 +182,7 @@ func TestGetAlbums(t *testing.T) {
 		Id:             1,
 		Title:          "awa",
 		Year:           2000,
-		Artist:         "awa",
+		Artist:         "Земфира",
 		ArtWork:        "awa",
 		TracksCount:    1,
 		TracksDuration: 1,
@@ -75,11 +191,19 @@ func TestGetAlbums(t *testing.T) {
 	albums = append(albums, album)
 
 	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
-		SELECT a.id, a.title, a.year, art.name, a.artwork, a.track_count, SUM(t.duration) as tracksDuration FROM albums a
-		LEFT JOIN artists art ON art.id = a.artist
-		JOIN tracks t on t.album = a.id
-		GROUP BY a.id, a.title, a.year, art.name, a.artwork, a.track_count
-		LIMIT 1
+		SELECT a.id,
+										       a.title,
+										       a.year,
+										       art.name,
+										       a.artwork,
+										       a.track_count,
+										       SUM(t.duration) as tracksDuration
+										FROM albums a
+										         LEFT JOIN artists art ON art.id = a.artist
+										         JOIN tracks t on t.album = a.id
+										WHERE art.name = 'Земфира'
+										GROUP BY a.id, a.title, a.year, art.name, a.artwork, a.track_count
+										LIMIT 1
 	`))).WillReturnRows(func() *sqlmock.Rows {
 		row := sqlmock.NewRows([]string{"id", "title", "year", "artist", "artwork", "track_count", "tracksDuration"})
 		row.AddRow(album.Id, album.Title, album.Year, album.Artist, album.ArtWork, album.TracksCount, album.TracksDuration)
@@ -110,7 +234,7 @@ func TestGetArtists(t *testing.T) {
 	artists = append(artists, artist)
 
 	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
-		SELECT * FROM artists LIMIT 1
+		SELECT artists.id, artists.name, artists.bio FROM artists LIMIT 1
 	`))).WillReturnRows(func() *sqlmock.Rows {
 		row := sqlmock.NewRows([]string{"id", "name", "bio"})
 		row.AddRow(artist.Id, artist.Name, artist.Bio)
@@ -135,13 +259,13 @@ func TestGetPlaylists(t *testing.T) {
 	playlist := models.Playlist{
 		Id:   1,
 		Name: "awa",
-		User:  1,
+		User: 1,
 	}
 	var playlists []models.Playlist
 	playlists = append(playlists, playlist)
 
 	mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
-		SELECT * FROM playlists LIMIT 1
+		SELECT playlists.id, playlists.title, playlists.user FROM playlists LIMIT 1
 	`))).WillReturnRows(func() *sqlmock.Rows {
 		row := sqlmock.NewRows([]string{"id", "name", "user"})
 		row.AddRow(playlist.Id, playlist.Name, playlist.User)
--- utils/musicSelections.go
+++ utils/musicSelections.go
@@ -4,6 +4,7 @@ import (
 	"2021_2_LostPointer/models"
 	"database/sql"
 	"fmt"
+	"math/rand"
 )
 
 const TracksSelectionLimit = 10
@@ -11,58 +12,149 @@ const AlbumsSelectionLimit = 4
 const PlaylistsSelectionLimit = 4
 const ArtistsSelectionLimit = 4
 
-func GetTracks(amount int, db *sql.DB) ([]models.Track, error) {
-	tracks := make([]models.Track, 0)
-	rows, err := db.Query(fmt.Sprintf(`SELECT DISTINCT ON (alb.title) t.id,
-												                              t.title,
-												                              art.name,
-												                              alb.title,
-												                              t.explicit,
-												                              g.name,
-												                              t.number,
-												                              t.file,
-												                              t.listen_count,
-												                              t.duration,
-												                              alb.artwork
-												FROM tracks t
-												         LEFT JOIN albums alb ON alb.id = t.album
-												         LEFT JOIN artists art ON art.id = t.artist
-												         LEFT JOIN genres g ON g.id = t.genre
-												WHERE RANDOM() < 0.5
-												LIMIT %d`, amount))
+func isGenresExist(genres []string, db *sql.DB) (bool, error) {
+	availableGenres := make(map[string]bool, 0)
+	rows, err := db.Query(`SELECT name FROM genres`)
+	if err != nil {
+		return false, err
+	}
+
+	var genre string
+	for rows.Next() {
+		if err := rows.Scan(&genre); err != nil {
+			return false, err
+		}
+		availableGenres[genre] = true
+	}
+
+	for _, genre := range genres {
+		if availableGenres[genre] == false {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func createTracksRequestWithGenres(genres []string) string {
+	baseRequest := `SELECT DISTINCT ON(tracks.album) tracks.id FROM tracks
+LEFT JOIN genres g on tracks.genre = g.id
+WHERE`
+
+	for _, genre := range genres {
+		baseRequest += fmt.Sprintf(" g.name='%s' OR", genre)
+	}
+
+	request := baseRequest[:len(baseRequest)-3]
+	return request
+}
+
+func createTracksRequestWithIds(ids []int) string {
+	baseRequest := `SELECT t.id,
+							t.title,
+							art.name,
+							alb.title,
+							t.explicit,
+							g.name,
+							t.number,
+							t.file,
+							t.listen_count,
+							t.duration,
+							alb.artwork
+							FROM tracks t
+							LEFT JOIN albums alb ON alb.id = t.album
+							LEFT JOIN artists art ON art.id = t.artist
+							LEFT JOIN genres g ON g.id = t.genre
+							WHERE t.id IN (`
+
+	for _, id := range ids {
+		baseRequest += fmt.Sprintf("%d, ", id)
+	}
+
+	request := baseRequest[:len(baseRequest)-2] + ")"
+
+	return request
+}
+
+func getTracksIdByGenre(db *sql.DB, genres []string) ([]int, error) {
+	isGenresCorrect, err := isGenresExist(genres, db)
+	if err != nil || isGenresCorrect == false {
+		return nil, err
+	}
+
+	request := createTracksRequestWithGenres(genres)
+	rows, err := db.Query(request)
 	if err != nil {
 		return nil, err
 	}
 
-	var track models.Track
+	tracksId := make([]int, 0)
+	var trackId int
+	for rows.Next() {
+		if err := rows.Scan(&trackId); err != nil {
+			return nil, err
+		}
+		tracksId = append(tracksId, trackId)
+	}
 
+	return tracksId, nil
+}
+
+func GetTracks(amount int, db *sql.DB, genres ...string) ([]models.Track, error) {
+	tracksRandom := make([]models.Track, 0)
+	tracksId, err := getTracksIdByGenre(db, genres)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(tracksId)
+	tracksIdRandom := make([]int, 0)
+	for i := 0; i < amount; {
+		newRandomTrackId := tracksId[rand.Intn(len(tracksId)-1)]
+		var isExist bool
+		for _, id := range tracksIdRandom {
+			if id == newRandomTrackId {
+				isExist = true
+			}
+		}
+		if !isExist {
+			tracksIdRandom = append(tracksIdRandom, newRandomTrackId)
+			i++
+		}
+	}
+	request := createTracksRequestWithIds(tracksIdRandom)
+
+	rows, err := db.Query(request)
+	if err != nil {
+		return nil, err
+	}
+	var track models.Track
 	for rows.Next() {
 		if err := rows.Scan(&track.Id, &track.Title, &track.Artist, &track.Album, &track.Explicit, &track.Genre,
 			&track.Number, &track.File, &track.ListenCount, &track.Duration, &track.Cover); err != nil {
 			return nil, err
 		}
-		tracks = append(tracks, track)
+		tracksRandom = append(tracksRandom, track)
 	}
 
-	return tracks, nil
+	return tracksRandom, nil
 }
 
 func GetAlbums(amount int, db *sql.DB) ([]models.Album, error) {
 	albums := make([]models.Album, 0)
-	rows, err := db.Query(fmt.Sprintf(`SELECT DISTINCT ON (u.title) *
-												FROM (SELECT DISTINCT ON (art.name) a.id,
-												                                    a.title,
-												                                    a.year,
-												                                    art.name,
-												                                    a.artwork,
-												                                    a.track_count,
-												                                    SUM(t.duration) as tracksDuration
-												      FROM albums a
-												               LEFT JOIN artists art ON art.id = a.artist
-												               JOIN tracks t on t.album = a.id
-												      WHERE RANDOM() <= 0.1
-												      GROUP BY a.id, a.title, a.year, art.name, a.artwork, a.track_count
-												      LIMIT %d) as u`, amount))
+	rows, err := db.Query(fmt.Sprintf(`SELECT a.id,
+										       a.title,
+										       a.year,
+										       art.name,
+										       a.artwork,
+										       a.track_count,
+										       SUM(t.duration) as tracksDuration
+										FROM albums a
+										         LEFT JOIN artists art ON art.id = a.artist
+										         JOIN tracks t on t.album = a.id
+										WHERE art.name = 'Земфира'
+										GROUP BY a.id, a.title, a.year, art.name, a.artwork, a.track_count
+										LIMIT %d`, amount))
 
 	if err != nil {
 		return nil, err
@@ -122,7 +214,7 @@ func GetSelectionForHomePage(db *sql.DB) (*models.SelectionFroHomePage, error) {
 	var selectionForHomePage = new(models.SelectionFroHomePage)
 	var err error
 
-	if selectionForHomePage.Tracks, err = GetTracks(TracksSelectionLimit, db); err != nil {
+	if selectionForHomePage.Tracks, err = GetTracks(TracksSelectionLimit, db, "Swing", "Jazz", "Rock", "Easy Listening", "Pop"); err != nil {
 		return nil, err
 	}
 	if selectionForHomePage.Albums, err = GetAlbums(AlbumsSelectionLimit, db); err != nil {

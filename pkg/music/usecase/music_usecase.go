ddb5d67d714467c8c4bca1488a572c6cbfb44d83
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-10-13 16:39:26 +0300

    Correct CreateTracksRequest

--- pkg/music/usecase/music_usecase.go
+++ pkg/music/usecase/music_usecase.go
@@ -5,6 +5,7 @@ import (
 	"2021_2_LostPointer/pkg/music"
 	"2021_2_LostPointer/pkg/music/repository"
 	"math/rand"
+	"strconv"
 )
 
 const TracksCollectionLimit = 10
@@ -15,10 +16,10 @@ const ArtistsCollectionLimit = 4
 var defaultGenres = []string{"Swing", "Jazz", "Rock", "Easy Listening", "Pop"}
 
 type MusicUseCase struct {
-	MusicRepository music.MusicRepositoryInterface
+	MusicRepository music.MusicRepositoryIFace
 }
 
-func NewMusicUseCase(musicRepository music.MusicRepositoryInterface) MusicUseCase {
+func NewMusicUseCase(musicRepository music.MusicRepositoryIFace) MusicUseCase {
 	return MusicUseCase{MusicRepository: musicRepository}
 }
 
@@ -43,7 +44,7 @@ func (musicUseCase MusicUseCase) GetMusicCollection() (*models.MusicCollection, 
 }
 
 func (musicUseCase MusicUseCase) GetTracksForCollection(amount int, genres []string) ([]models.Track, error) {
-	requestWithGenres, err := musicUseCase.MusicRepository.CreateTracksRequestWithParameters(repository.GettingWithGenres, genres, repository.DistinctOnAlbums)
+	requestWithGenres, err := musicUseCase.MusicRepository.CreateTracksRequestWithParameters(repository.GettingWithGenres, genres, repository.DistinctOnNone)
 	if err != nil {
 		return nil, err
 	}
@@ -61,9 +62,9 @@ func (musicUseCase MusicUseCase) GetTracksForCollection(amount int, genres []str
 		}
 
 	}
-	randomTracksIDArray := make([]int64, 0)
+	randomTracksIDArray := make([]string, 0)
 	for key := range randomTracksIDMap {
-		randomTracksIDArray = append(randomTracksIDArray, key)
+		randomTracksIDArray = append(randomTracksIDArray, strconv.FormatInt(key, 10))
 	}
 
 	request, err := musicUseCase.MusicRepository.CreateTracksRequestWithParameters(repository.GettingWithID, randomTracksIDArray, repository.DistinctOnAlbums)

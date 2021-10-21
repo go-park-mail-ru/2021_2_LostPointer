f59882a1f8ef055c715a7ed2f72f3e8499e72623
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-10-21 21:19:51 +0300

    Add limit for selections

--- internal/artist/repository.go
+++ internal/artist/repository.go
@@ -5,6 +5,6 @@ import "2021_2_LostPointer/internal/models"
 //go:generate moq -out ../mock/artist_repo_db_mock.go -pkg mock . ArtistRepository:MockArtistRepository
 type ArtistRepository interface {
 	Get(id int) (*models.Artist, error)
-	GetTracks(id int, isAuthorized bool) ([]models.Track, error)
-	GetAlbums(id int) ([]models.Album, error)
+	GetTracks(id int, isAuthorized bool, amount int) ([]models.Track, error)
+	GetAlbums(id int, amount int) ([]models.Album, error)
 }

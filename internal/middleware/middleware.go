6af5d2457259217b4b38eca52bf2e44686a74536
Виталий <vitalyerhsov2001@gmail.com>
2021-10-22 17:58:20 +0300

    Add csrf

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,11 +1,15 @@
 package middleware
 
 import (
+	"2021_2_LostPointer/internal/csrf"
 	"2021_2_LostPointer/internal/models"
 	"2021_2_LostPointer/internal/users"
+	"errors"
 	"github.com/labstack/echo"
 	"github.com/satori/go.uuid"
 	"go.uber.org/zap"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -24,6 +28,7 @@ func NewMiddlewareHandler(logger *zap.SugaredLogger, userUseCase users.UserUseCa
 func (middleware Middleware) InitMiddlewareHandlers(server *echo.Echo) {
 	server.Use(middleware.CheckAuthorization)
 	server.Use(middleware.AccessLog)
+	server.Use(middleware.CSRF)
 }
 
 func (middleware Middleware) CheckAuthorization(next echo.HandlerFunc) echo.HandlerFunc {
@@ -73,5 +78,44 @@ func (middleware Middleware) AccessLog(next echo.HandlerFunc) echo.HandlerFunc {
 		)
 
 		return err
+	}
+}
+
+func (middleware Middleware) CSRF(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(rwContext echo.Context) error {
+		if rwContext.Request().RequestURI == "/api/v1/user/settings" || rwContext.Request().Method == "PATCH" {
+			cookie, err := rwContext.Cookie("Session_cookie")
+			if err != nil {
+				middleware.logger.Debug(
+					zap.String("COOKIE", errors.New("cookie expired").Error()),
+					zap.Int("ANSWER STATUS", http.StatusUnauthorized),
+				)
+
+				return rwContext.JSON(http.StatusUnauthorized, &models.Response{
+					Status: http.StatusUnauthorized,
+					Message: "Cookie expired",
+				})
+			}
+
+			tokenReq := rwContext.Request().Header.Get("X-CSRF-Token")
+			log.Println(tokenReq)
+
+			isValidCsrf, err := csrf.Tokens.Check(cookie.Value, tokenReq)
+
+			if err != nil {
+				return rwContext.JSON(http.StatusForbidden, &models.Response{
+					Status: http.StatusForbidden,
+					Message: "Cookie expired",
+				})
+			}
+
+			if !isValidCsrf  {
+				return rwContext.JSON(http.StatusForbidden, &models.Response{
+					Status: http.StatusForbidden,
+					Message: "Cookie expired",
+				})
+			}
+		}
+		return next(rwContext)
 	}
 }

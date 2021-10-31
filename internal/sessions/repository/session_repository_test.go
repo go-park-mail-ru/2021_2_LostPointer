8dfb1ea7931bee9f5f50e8b049471136a94aee0e
Виталий <vitalyerhsov2001@gmail.com>
2021-10-31 23:38:36 +0300

    Fix tests after AK

--- internal/sessions/repository/session_repository_test.go
+++ internal/sessions/repository/session_repository_test.go
@@ -1,36 +1,47 @@
 package repository
 
 import (
+	"2021_2_LostPointer/internal/utils/constants"
 	"errors"
 	"github.com/go-redis/redismock/v8"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"time"
 )
 
 func TestSessionRepository_CreateSession(t *testing.T) {
 	db, mock := redismock.NewClientMock()
 	repository := NewSessionRepository(db)
 
-	var cookieValue = "awa"
-	var id = 12345
+	type inputStruct struct {
+		id 			int
+		cookieValue string
+	}
 	tests := []struct {
 		name        string
 		mock        func()
+		input 		inputStruct
 		expected    error
 		expectedErr bool
 	}{
 		{
-			name: "successes",
+			name: "Successfully stored session",
 			mock: func() {
-				mock.ExpectSet(cookieValue, id, time.Hour).SetVal("")
+				mock.ExpectSet("cookie_value", 1, constants.CookieLifetime).SetVal("")
+			},
+			input: inputStruct{
+				id: 1,
+				cookieValue: "cookie_value",
 			},
 			expectedErr: false,
 		},
 		{
-			name: "error",
+			name: "Error occurred in redis.Set",
 			mock: func() {
-				mock.ExpectSet(cookieValue, id, time.Hour).SetErr(errors.New("awa"))
+				mock.ExpectSet("cookie_value", 1, constants.CookieLifetime).SetErr(errors.New("error"))
+			},
+			input: inputStruct{
+				id: 1,
+				cookieValue: "cookie_value",
 			},
 			expectedErr: true,
 		},
@@ -38,7 +49,7 @@ func TestSessionRepository_CreateSession(t *testing.T) {
 	for _, testCase := range tests {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mock()
-			err := repository.CreateSession(id, cookieValue)
+			err := repository.CreateSession(testCase.input.id, testCase.input.cookieValue)
 			if testCase.expectedErr {
 				assert.Error(t, err)
 			} else {
@@ -52,38 +63,38 @@ func TestSessionRepository_GetUserIdByCookie(t *testing.T) {
 	db, mock := redismock.NewClientMock()
 	repository := NewSessionRepository(db)
 
-	var cookieValue = "awa"
-	var id = 12345
-	var idStr = "12345"
 	tests := []struct {
 		name        string
 		mock        func()
-		expected    error
+		input 		string
+		expected   	int
 		expectedErr bool
 	}{
 		{
-			name: "successes",
+			name: "Successfully returned user id by cookie value",
 			mock: func() {
-				mock.ExpectGet(cookieValue).SetVal(idStr)
+				mock.ExpectGet("cookie_value").SetVal("1")
 			},
-			expectedErr: false,
+			input: "cookie_value",
+			expected: 1,
 		},
 		{
-			name: "error",
+			name: "Error occurred in redis.Get",
 			mock: func() {
-				mock.ExpectGet(cookieValue).SetErr(errors.New("awa"))
+				mock.ExpectGet("cookie_value").SetErr(errors.New("error"))
 			},
+			input: "cookie_value",
 			expectedErr: true,
 		},
 	}
 	for _, testCase := range tests {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mock()
-			res, err := repository.GetUserIdByCookie(cookieValue)
+			res, err := repository.GetUserIdByCookie(testCase.input)
 			if testCase.expectedErr {
 				assert.Error(t, err)
 			} else {
-				assert.Equal(t, id, res)
+				assert.Equal(t, testCase.expected, res)
 				assert.NoError(t, err)
 			}
 		})
@@ -94,32 +105,33 @@ func TestSessionRepository_DeleteSession(t *testing.T) {
 	db, mock := redismock.NewClientMock()
 	repository := NewSessionRepository(db)
 
-	var cookieValue = "awa"
 	tests := []struct {
 		name        string
 		mock        func()
 		expected    error
+		input 		string
 		expectedErr bool
 	}{
 		{
-			name: "successes",
+			name: "Successfully deleted session",
 			mock: func() {
-				mock.ExpectDel(cookieValue).SetVal(1)
+				mock.ExpectDel("cookie_value").SetVal(1)
 			},
-			expectedErr: false,
+			input: "cookie_value",
 		},
 		{
-			name: "error",
+			name: "Error occurred in redis.Del",
 			mock: func() {
-				mock.ExpectDel(cookieValue).SetErr(errors.New("awa"))
+				mock.ExpectDel("cookie_value").SetErr(errors.New("error"))
 			},
+			input: "cookie_value",
 			expectedErr: true,
 		},
 	}
 	for _, testCase := range tests {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mock()
-			err := repository.DeleteSession(cookieValue)
+			err := repository.DeleteSession(testCase.input)
 			if testCase.expectedErr {
 				assert.Error(t, err)
 			} else {

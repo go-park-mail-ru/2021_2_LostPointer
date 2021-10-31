902f22828bbd371926983a3774542f4d2a07a2c3
Виталий <vitalyerhsov2001@gmail.com>
2021-11-01 00:13:06 +0300

    Removed /queues

--- internal/queues/usecase.go
+++
@@ -1,8 +0,0 @@
-package queues
-
-import "2021_2_LostPointer/internal/models"
-
-type QueueUsecase interface {
-	StoreQueue(int, *models.Queue) *models.CustomError
-	GetQueue(int) (*models.Queue, *models.CustomError)
-}

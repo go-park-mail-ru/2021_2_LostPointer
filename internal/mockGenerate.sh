b61ed4b127ad5294865ce4a033b0cbb4f0d926d8
Виталий <vitalyerhsov2001@gmail.com>
2021-11-02 18:21:09 +0300

    Fix bash script after

--- internal/mockGenerate.sh
+++ internal/mockGenerate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-moq -out mock/album_repo_db_mock.go -pkg mock album AlbumRepository:MockAlbumRepository
-moq -out mock/album_usecase_db_mock.go -pkg mock album AlbumUseCase:MockAlbumUseCase
+go generate moq -out ./mock/album_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/album AlbumRepository:MockAlbumRepository
+go generate moq -out ./mock/album_usecase_db_mock.go -pkg mock 2021_2_LostPointer/internal/album AlbumUseCase:MockAlbumUseCase
 
-moq -out mock/artist_repo_db_mock.go -pkg mock artist ArtistRepository:MockArtistRepository
-moq -out mock/artist_usecase_db_mock.go -pkg mock artist ArtistUseCase:MockArtistUseCase
+go generate moq -out ./mock/artist_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/artist ArtistRepository:MockArtistRepository
+go generate moq -out ./mock/artist_usecase_db_mock.go -pkg mock 2021_2_LostPointer/internal/artist ArtistUseCase:MockArtistUseCase
 
-moq -out mock/playlist_repo_db_mock.go -pkg mock playlist PlaylistRepository:MockPlayListRepository
-moq -out mock/playlist_usecase_db_mock.go -pkg mock playlist PlaylistUseCase:MockPlaylistUseCase
+go generate moq -out ./mock/playlist_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/playlist PlaylistRepository:MockPlayListRepository
+go generate moq -out ./mock/playlist_usecase_db_mock.go -pkg mock 2021_2_LostPointer/internal/playlist PlaylistUseCase:MockPlaylistUseCase
 
-moq -out mock/sessions_repo_db_mock.go -pkg mock sessions SessionRepository:MockSessionRepository
+go generate moq -out ./mock/sessions_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/sessions SessionRepository:MockSessionRepository
 
-moq -out mock/track_repo_db_mock.go -pkg mock track TrackRepository:MockTrackRepository
-moq -out mock/track_usecase_mock.go -pkg mock track TrackUseCase:MockTrackUseCase
+go generate moq -out ./mock/track_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/track TrackRepository:MockTrackRepository
+go generate moq -out ./mock/track_usecase_mock.go -pkg mock 2021_2_LostPointer/internal/track TrackUseCase:MockTrackUseCase
 
-moq -out mock/user_repo_db_mock.go -pkg mock users UserRepository:MockUserRepository
-moq -out mock/user_usecase_mock.go -pkg mock users UserUseCase:MockUserUseCase
+go generate moq -out ./mock/user_repo_db_mock.go -pkg mock 2021_2_LostPointer/internal/users UserRepository:MockUserRepository
+go generate moq -out ./mock/user_usecase_mock.go -pkg mock 2021_2_LostPointer/internal/users UserUseCase:MockUserUseCase
 
-moq -out mock/avatar_repository_mock.go -pkg mock utils/images AvatarRepositoryIFace:MockAvatarRepositoryIFace
+go generate moq -out ./mock/avatar_repository_mock.go -pkg mock 2021_2_LostPointer/internal/utils/images AvatarRepositoryIFace:MockAvatarRepositoryIFace
 
-moq -out mock/session_checker_mock.go -pkg mock microservices/authorization/delivery SessionCheckerClient:MockSessionCheckerClient
+go generate moq -out ./mock/session_checker_mock.go -pkg mock 2021_2_LostPointer/internal/microservices/authorization/delivery SessionCheckerClient:MockSessionCheckerClient

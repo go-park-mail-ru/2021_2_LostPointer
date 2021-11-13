d9c950beb4374fc4bac3dd151817ae098860b3e7
Виталий <vitalyerhsov2001@gmail.com>
2021-11-13 20:02:35 +0300

    LOS-82 update: implement album page

--- internal/models/album.go
+++ internal/models/album.go
@@ -1,6 +1,8 @@
 package models
 
-import "2021_2_LostPointer/internal/microservices/music/proto"
+import (
+	"2021_2_LostPointer/internal/microservices/music/proto"
+)
 
 type Album struct {
 	ID             int64  `json:"id,omitempty"`
@@ -11,6 +13,44 @@ type Album struct {
 	TracksCount    int64  `json:"tracks_count,omitempty"`
 	TracksDuration int64  `json:"tracks_duration,omitempty"`
 	ArtworkColor   string `json:"artwork_color,omitempty"`
+}
+
+type AlbumPage struct {
+	ID             int64        `json:"id,omitempty"`
+	Title          string       `json:"title,omitempty"`
+	Year           int64        `json:"year,omitempty"`
+	Artwork        string       `json:"artwork,omitempty"`
+	TracksCount    int64        `json:"tracks_count,omitempty"`
+	TracksDuration int64        `json:"tracks_duration,omitempty"`
+	ArtworkColor   string       `json:"artwork_color,omitempty"`
+	Artist         Artist       `json:"artist"`
+	Tracks         []TrackAlbum `json:"tracks,omitempty"`
+}
+
+func (a *AlbumPage) BindProtoAlbumPage(album *proto.AlbumPageResponse) {
+	tracks := make([]TrackAlbum, 0)
+	for _, t := range album.Tracks {
+		var track TrackAlbum
+		track.BindProtoAlbumTrack(t)
+		tracks = append(tracks, track)
+	}
+
+	bindedAlbum := &AlbumPage{
+		ID:             album.AlbumID,
+		Title:          album.Title,
+		Year:           album.Year,
+		Artwork:        album.Artwork,
+		TracksCount:    album.TracksCount,
+		TracksDuration: album.TracksDuration,
+		ArtworkColor:   album.ArtworkColor,
+		Artist: Artist{
+			ID:   album.Artist.ID,
+			Name: album.Artist.Name,
+		},
+		Tracks: tracks,
+	}
+
+	*a = *bindedAlbum
 }
 
 func (a *Album) BindProtoAlbum(album *proto.Album) {

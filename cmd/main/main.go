5977a7373ea85675666c971e74aeebe2e5ecfcc3
Виталий <vitalyershov2001@gmail.com>
2021-11-17 20:13:13 +0300

    Added server.Static for /tracks

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -90,5 +90,7 @@ func main() {
 	appHandler.Init(server)
 	middlewareHandler.InitMiddlewareHandlers(server)
 
+	server.Static("/tracks", os.Getenv("TRACKS_PATH"))
+
 	server.Logger.Fatal(server.Start(fmt.Sprintf("%s:%s", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))))
 }

4c85243ec48bea2cd97cdd28646e54553e2c0776
Виталий <vitalyershov2001@gmail.com>
2021-11-17 12:53:30 +0300

    LOS-91 update: added image for playlist

--- internal/microservices/playlists/repository.go
+++ internal/microservices/playlists/repository.go
@@ -3,10 +3,10 @@ package playlists
 import "2021_2_LostPointer/internal/microservices/playlists/proto"
 
 type Storage interface {
-	CreatePlaylist(int64, string) (*proto.CreatePlaylistResponse, error)
-	UpdatePlaylist(int64, string) error
+	CreatePlaylist(int64, string, string) (*proto.CreatePlaylistResponse, error)
+	UpdatePlaylist(int64, string, string) error
+	GetOldArtwork(int64) (string, error)
 	DeletePlaylist(int64) error
-	UserPlaylists(int64) ([]*proto.Playlist, error)
 	AddTrack(int64, int64) error
 	DeleteTrack(int64, int64) error
 	IsAdded(int64, int64) (bool, error)

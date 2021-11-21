d4f8e6f42e32ab9efabba970eb12199abbbbf48b
saptded <alexey.kosenko2880@gmail.com>
2021-11-21 19:01:00 +0300

    update metrics.sh

--- internal/monitoring/metrics.sh
+++ internal/monitoring/metrics.sh
@@ -6,8 +6,8 @@
 
 echo "running prometheus on port 9090"
 # shellcheck disable=SC2154
-sudo docker run -p 9091:9090 -d --name prometheus --net=host -v $PWD/prometheus:/etc/config prom/prometheus --config.file=/etc/config/prometheus.yml
+sudo docker run -p 9090:9090 -d --name prometheus --net=host -v $PWD/prometheus:/etc/config prom/prometheus --config.file=/etc/config/prometheus.yml
 echo "running node exporter on port 9100"
 sudo docker run -p 9100:9100 -d --name node_exporter --net=host -v $PWD/node_exporter:/etc/config prom/node-exporter --path.rootfs=/etc/config
 echo "running grafana on port 3000"
-sudo docker run -d -p 3001:3000 --name grafana --net=host -v $PWD/grafana/grafana.ini:/etc/grafana/grafana.ini grafana/grafana
+sudo docker run -d -p 3000:3000 --name grafana --net=host -v $PWD/grafana/grafana.ini:/etc/grafana/grafana.ini grafana/grafana

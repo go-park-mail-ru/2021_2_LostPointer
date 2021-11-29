424aec5b8c6bc98b5a48663c07be3882a30243a1
Leonid <belyaevleonid2000@mail.ru>
2021-11-29 16:16:03 +0300

    main_fix: Цвет обложки при отдаче трека для мобильного плеера

--- internal/microservices/music/repository/music.go
+++ internal/microservices/music/repository/music.go
@@ -20,7 +20,7 @@ func NewMusicStorage(db *sql.DB) *MusicStorage {
 func (storage *MusicStorage) RandomTracks(amount int64, isAuthorized bool) (*proto.Tracks, error) {
 	query := `SELECT ` +
 		wrapper.Wrapper([]string{"id", "title", "explicit", "number", "file", "listen_count", "duration", "lossless"}, "t") + ", " +
-		wrapper.Wrapper([]string{"id", "title", "artwork"}, "alb") + ", " +
+		wrapper.Wrapper([]string{"id", "title", "artwork", "artwork_color"}, "alb") + ", " +
 		wrapper.Wrapper([]string{"id", "name"}, "art") + ", " +
 		wrapper.Wrapper([]string{"name"}, "g") +
 		`
@@ -48,7 +48,7 @@ func (storage *MusicStorage) RandomTracks(amount int64, isAuthorized bool) (*pro
 		track.Album = &proto.Album{}
 		track.Artist = &proto.Artist{}
 		if err = rows.Scan(&track.ID, &track.Title, &track.Explicit, &track.Number, &track.File, &track.ListenCount,
-			&track.Duration, &track.Lossless, &track.Album.ID, &track.Album.Title, &track.Album.Artwork, &track.Artist.ID,
+			&track.Duration, &track.Lossless, &track.Album.ID, &track.Album.Title, &track.Album.Artwork, &track.Album.ArtworkColor, &track.Artist.ID,
 			&track.Artist.Name, &track.Genre); err != nil {
 			return nil, err
 		}

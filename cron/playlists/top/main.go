d92b5e2eb2fed00a26ed8f1fb4b02e01c440a098
Виталий <vitalyershov2001@gmail.com>
2021-12-24 15:39:49 +0300

    LOS-137 updated: implemented

--- cron/playlists/top/main.go
+++ cron/playlists/top/main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"2021_2_LostPointer/internal/constants"
+	"context"
 	"database/sql"
 	"fmt"
-	"github.com/go-redis/redis/v8"
 	"github.com/robfig/cron"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
+
+	"github.com/go-redis/redis/v8"
+	_ "github.com/lib/pq"
 )
 
 const (
-	redisKey = "top10"
-	userID = 261
-	title = "LostPointer top 10"
-	artwork = "top_10_100px.webp"
+	redisKey     = "top10"
+	userID       = 261
+	title        = "LostPointer top 10"
+	artwork      = "top_10_100px.webp"
 	artworkColor = "#e60f5a"
-	isPublic = true
+	isPublic     = true
 )
 
 func ConnectRedis() *redis.Client {
@@ -30,7 +35,7 @@ func ConnectRedis() *redis.Client {
 	redisConnection := redis.NewClient(&redis.Options{
 		Addr:     AddrConfig,
 		Password: os.Getenv("REDIS_PASS"),
-		DB:       1,
+		DB:       3,
 	})
 
 	return redisConnection
@@ -45,6 +50,7 @@ func ConnectDatabase() *sql.DB {
 		os.Getenv("DBPORT"),
 		os.Getenv("DBNAME"),
 	)
+
 	database, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatalln("NO CONNECTION TO DATABASE", err.Error())
@@ -74,31 +80,107 @@ func getTopTracks(db *sql.DB) ([]int, error) {
 		if err := rows.Scan(&id); err != nil {
 			return tracksID, err
 		}
-
 		tracksID = append(tracksID, id)
+	}
+	err = rows.Err()
+	if err != nil {
+		return tracksID, err
 	}
 
 	return tracksID, nil
 }
 
-func UpdatePlaylist() {
+func initializePlaylist(db *sql.DB, tracksID []int) (int, error) {
+	query := `INSERT INTO playlists(title, user_id, artwork, artwork_color, is_public) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+
+	var id int
+	err := db.QueryRow(query, title, userID, artwork, artworkColor, isPublic).Scan(&id)
+	if err != nil {
+		return id, err
+	}
+
+
+	query = `INSERT INTO playlist_tracks(playlist, track) VALUES ($1, $2)`
+	for _, trackID := range tracksID {
+		_, err := db.Exec(query, id, trackID)
+		if err != nil {
+			return id, err
+		}
+	}
+
+	return id, nil
+}
+
+func updatePlaylist(db *sql.DB, playlistID string, tracksID []int) error {
+	query := `SELECT id FROM playlist_tracks WHERE playlist = $1`
+
+	rows, err := db.Query(query, playlistID)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Fatal("Error occurred during closing rows")
+		}
+	}()
+
+	var currentID int
+	id := make([]int, 0)
+	for rows.Next() {
+		if err := rows.Scan(&currentID); err != nil {
+			return err
+		}
+
+		id = append(id, currentID)
+	}
+
+	query = `UPDATE playlist_tracks SET track=$1 WHERE id=$2`
+	for i := 0; i < len(tracksID); i++ {
+		_, err := db.Exec(query, tracksID[i], id[i])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func CreatePlaylist() {
 	log.Println("Cron called function")
 
 	dbConn := ConnectDatabase()
-	// redisConn := ConnectRedis()
+	redisConn := ConnectRedis()
 
 	tracksID, err := getTopTracks(dbConn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(tracksID)
+	playlistID, err := redisConn.Get(context.Background(), redisKey).Result()
+	if err == redis.Nil {
+		createdPlaylistID, err := initializePlaylist(dbConn, tracksID)
+		playlistID = strconv.Itoa(createdPlaylistID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = redisConn.Set(context.Background(), redisKey, playlistID, constants.CookieLifetime).Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		err = updatePlaylist(dbConn, playlistID, tracksID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	
 }
 
 func main() {
 	c := cron.New()
 
-	err := c.AddFunc("1 * * * * *", UpdatePlaylist)
+	err := c.AddFunc("5 * * * *", CreatePlaylist)
 	if err != nil {
 		log.Fatal(err)
 	}
